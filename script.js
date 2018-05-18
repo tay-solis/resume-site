ab9c40562fe7a98bb8edce2833a2ddecb22c2e91
Tay J Solis <tayjsolis@gmail.com>
2018-05-18 16:36:05 -0700

    added about and prepared nav/footer on all pages with links

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+    $('.welcome').fadeIn(1000).css('display', 'block');
+    $('.container').fadeIn(1000).css('display', 'block');
+});
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.reveal', { duration: 400 }, 50);
+
 $('.dropdown').click(function(){
   $(this).children('.dropdown-content').slideToggle('fast');
 });
